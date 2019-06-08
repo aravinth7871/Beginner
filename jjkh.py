12bd773af43d64f7213011e2b25162de1872ab77
aravinth7871 <51469895+aravinth7871@users.noreply.github.com>
2019-06-08 14:35:38 +0530

    Create jjkh.py

---
+++ jjkh.py
@@ -0,0 +1,12 @@
+k = int(input())
+w = list(map(int,input().split()))
+y = []
+for i in range(0,k):
+	if w[i]==i:
+		y.append(i)
+y.sort()
+if(len(y)!=0):
+	y = [str(x) for x in y]
+	print(" ".join(y))
+else:
+	print("-1")
